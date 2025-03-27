d65bf67db3c793ee464487c50bda9c9cc2cd8386
David Beshensky <beshenskydave@gmail.com>
2025-03-27 11:02:47 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Beatrite server is live!');
+});
+
+app.post('/device-data', (req, res) => {
+  console.log('Received data:', req.body);
+  res.send({ status: 'ok' });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
